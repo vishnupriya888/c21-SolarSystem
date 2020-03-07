1520e74172c8494f398bd91bc1d1066ec6c4064f
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-03-07 17:16:56 +0530

    Update sketch.js
    planets

--- sketch.js
+++ sketch.js
@@ -1,9 +1,23 @@
+var sun,mercury,venus,mars,earth,jupiter,saturn,uranus,neptune,pluto;
+var sunImg, mercuryImg, venusImg, earthImg, marsImg, jupeterImg, saturnImg, uranusImg, neptuneImg,plutoImg;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1200,800);
+
+  sun = createSprite(10,10, 200, 200);
+  mercury = createSprite(250,100,40,40);
+  venus = createSprite(350,150,30,30);
+  earth = createSprite(450,200,40,40);
+  mars = createSprite(550,250,40,40);
+  earth = createSprite(650,300,30,30);
+  jupiter = createSprite(750,350,30,30);
+  saturn = createSprite(850,450,40,40);
+  uranus = createSprite(950,550,30,30);
+  neptune = createSprite(1050,650,40,40);
+  pluto = createSprite(1150,700,30,30);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0,0,0);  
   drawSprites();
 }
